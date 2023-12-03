a2a7ae1af031fbc4ff2cdb01136083607c0cf782
RZJing5 <118409482+RZJing5@users.noreply.github.com>
2023-12-03 17:41:19 -0600

    Update dishes_star.R

--- code/dishes_star.R
+++ code/dishes_star.R
@@ -26,19 +26,33 @@ results_df$category <- rownames(results_df)
 library(ggplot2)
 
 
+diytheme <- theme(plot.title=element_text(face="bold.italic",
+                                          size="14", color="brown"),
+                  axis.title=element_text(face="bold.italic",
+                                          size=10, color="brown"),
+                  axis.text=element_text(face="bold", size=9,
+                                         color="darkblue"),
+                  panel.background=element_rect(fill="white",
+                                                color="darkblue"),
+                  panel.grid.major.y=element_line(color="grey",
+                                                  linetype=1),
+                  panel.grid.minor.y=element_line(color="grey",
+                                                  linetype=2),
+                  panel.grid.minor.x=element_blank(),
+                  legend.position="top") 
+
 ggplot(data = results_df, aes(x = category, y = mu, group = 1)) +
   geom_point(aes(colour = category, size = sd)) + 
   geom_line() +
   scale_colour_manual(values = rainbow(n = length(unique(results_df$category)))) + 
   scale_size_continuous(name = "Standard Deviation") + 
-  ggtitle("Mean and Standard Deviation of Food Categories") +
+  ggtitle("Mean and Standard Deviation of Food Categories (Off-Center & Low Price)") +
   geom_hline(yintercept = mean_dish, color = "red")+
   xlab(" ") +
   ylab("Mean Value") +
-  theme_minimal()+
+  diytheme+
   theme(legend.position = "bottom") +
   guides(size = FALSE)+
   guides(colour = FALSE)
-
 
 
